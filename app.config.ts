bf5235fbf5101d7e723859a6bcf691c9d2db4218
djara88 <d.jarazerene@gmail.com>
2026-09-11 22:46:18 -0300

    release: set Android beta version 0.5.1

--- app.config.ts
+++ app.config.ts
@@ -11,7 +11,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     name: 'Lestra After',
     slug: 'lestra-after',
     scheme: 'lestraafter',
-    version: '0.5.0',
+    version: '0.5.1',
     orientation: 'portrait',
     userInterfaceStyle: 'light',
     newArchEnabled: true,
@@ -21,6 +21,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     },
     android: {
       package: 'app.lestra.after',
+      versionCode: 2,
       adaptiveIcon: { backgroundColor: '#FFF8F1' },
     },
     plugins: [
